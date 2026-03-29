a2fbc559317de264f9b0fd5c09d76e3d3396b9ce
renigoms <renan.nic28@gmail.com>
2026-03-29 12:18:10 -0300

    admin area config

--- django1/core/admin.py
+++ django1/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from core.models import Product, Client
+
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("stock_quantity","name", "price")
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Client)
--- django1/core/models.py
+++ django1/core/models.py
@@ -7,7 +7,13 @@ class Product(models.Model):
     price = models.DecimalField('price', decimal_places=2, max_digits=10)
     stock_quantity = models.IntegerField('stock_quantity')
 
+    def __str__(self):
+        return self.name
+
 class Client(models.Model):
     name = models.CharField('name', max_length=100)
     email = models.EmailField('email', max_length=100)
     surname = models.CharField('surname', max_length=100)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
